0150e58ed9a9ace098b1926f83f3582c1bdeee59
rileykeane <rileykeane98@gmail.com>
2022-10-05 21:02:29 +1100

    Add CodeScanner and display ISBN when book is scanned

--- smart-library-app/Home/HomeView.swift
+++ smart-library-app/Home/HomeView.swift
@@ -6,12 +6,42 @@
 //
 
 import SwiftUI
+import CodeScanner
 
 struct HomeView: View {
+    
+    @State private var isShowingScanner = false
+    @State private var scannedISBN = ""
+    
     var body: some View {
-        Button("Scan") {
-            print("Hello, world")
+        NavigationStack {
+            VStack {
+                Button("Scan your book") {
+                    isShowingScanner = true
+                }
+                Text(scannedISBN)
+                    .padding(10)
+                
+            }
+            .navigationTitle("Home")
         }
+        .sheet(isPresented: $isShowingScanner) {
+            CodeScannerView(codeTypes: [.ean13], simulatedData: "9780804139298", completion: handleScan)
+        }
+        
+    }
+    
+    func handleScan(result: Result<ScanResult, ScanError>) {
+        isShowingScanner = false
+        
+        switch result {
+        case .success(let result):
+            scannedISBN = result.string
+        case .failure(let error):
+            // TODO: RK - Error handling
+            print("Something went wrong... \(error.localizedDescription)")
+        }
+        
     }
 }
 
