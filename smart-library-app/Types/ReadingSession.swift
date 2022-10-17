42624e805bc2684e4d2d1d159b477c4eb9b436ef
emmapainter <emma.r.painter@gmail.com>
2022-10-17 11:39:56 +1100

    ReadingSession type

---
+++ smart-library-app/Types/ReadingSession.swift
@@ -0,0 +1,16 @@
+//
+//  ReadingSession.swift
+//  smart-library-app
+//
+//  Created by Emma Painter on 17/10/2022.
+//
+
+import Foundation
+
+struct ReadingSession {
+    var startTime: Date
+    var endTime: Date?
+    var numberOfPages: Int?
+    var bookISBN13: String
+    var bookmarkId: String
+}
