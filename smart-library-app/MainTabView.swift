fd5673cca9ff5020e2df9b3e2501a731221e1eb3
rileykeane <rileykeane98@gmail.com>
2022-10-20 17:59:11 +1100

    Set page number WIP

--- smart-library-app/MainTabView.swift
+++ smart-library-app/MainTabView.swift
@@ -7,10 +7,32 @@
 
 import SwiftUI
 
-struct MainTabView: View {
+struct MainTabView: View, BluetoothControllerDelegate {
     @EnvironmentObject private var user: User
+    @State var showingPageAlert = false
+    @State var pageNumber = ""
+    var deviceUUID: UUID?
     
     var body: some View {
+        ContentView().alert("Important message", isPresented: $showingPageAlert) {
+            TextField("Enter your page number", text: $pageNumber)
+            Button("OK", role: .cancel) {
+                guard let deviceUUID = deviceUUID, let readingBook = user.getReadingBookFromBookmarkId(bookmarkId: deviceUUID.uuidString) else {
+                    Swift.print("Tried to update reading session for a book")
+                    return
+                }
+                
+                Task {
+                    do {
+                        print(pageNumber)
+                        try await user.stopReadingSession(readingBook: readingBook, pages: 10)
+                    } catch let error {
+                        print(error)
+                    }
+                    
+                }
+            }
+        }
         TabView {
             HomeView()
                 .tabItem {
@@ -26,6 +48,7 @@ struct MainTabView: View {
                 }
         }
         .onAppear {
+            BluetoothController.shared.addDelegate(delegate: self)
             Task {
                 do {
                     try await user.fetchUserData()
@@ -33,6 +56,32 @@ struct MainTabView: View {
                     print(error)
                 }
             }
+        }
+    }
+    
+    @MainActor mutating func bluetoothDeviceDidSendData(deviceUUID: UUID, data: String) {
+        print("Device: \(deviceUUID.description)")
+        print("Data: \(data)")
+        
+        guard let readingBook = user.getReadingBookFromBookmarkId(bookmarkId: deviceUUID.uuidString) else {
+            Swift.print("Tried to update reading session for a book")
+            return
+        }
+        
+        if data == "1" {
+            Task {
+                do {
+                    try await user.startReadingSession(readingBook: readingBook)
+                } catch let error {
+                    print(error)
+                }
+            }
+        }
+        
+        if data == "0" {
+            print("Stoped reading")
+            self.deviceUUID = deviceUUID
+            showingPageAlert = true
         }
     }
 }
--- smart-library-app/smart_library_appApp.swift
+++ smart-library-app/smart_library_appApp.swift
@@ -10,10 +10,9 @@ import Firebase
 import FirebaseAuth
 
 @main
-struct smart_library_appApp: App, BluetoothControllerDelegate {
+struct smart_library_appApp: App {
     
     var user: User
-    @State var showingPageAlert = true
     @State var pageNumber: Int?
 
     init(){
@@ -27,7 +26,6 @@ struct smart_library_appApp: App, BluetoothControllerDelegate {
         self.user = User()
         
         // Start bluetooth paring when app opens
-        BluetoothController.shared.addDelegate(delegate: self)
         let _ = BluetoothController.shared
         
         if Auth.auth().currentUser != nil {
@@ -37,54 +35,13 @@ struct smart_library_appApp: App, BluetoothControllerDelegate {
     
     var body: some Scene {
         
-        WindowGroup {
-//            ContentView().alert("Important message", isPresented: $showingPageAlert) {
-//                Button("OK", role: .cancel) {
-//                }
-//            }
-            
+        WindowGroup {            
             if user.loggedIn {
                 MainTabView()
                 .environmentObject(self.user)
             } else {
                 SignInView()
                 .environmentObject(self.user)
-            }
-        }
-    }
-    
-    func showAlert() {
-        self.showingPageAlert = true
-    }
-    
-    @MainActor func bluetoothDeviceDidSendData(deviceUUID: UUID, data: String) {
-        print("Device: \(deviceUUID.description)")
-        print("Data: \(data)")
-        
-        guard let readingBook = user.getReadingBookFromBookmarkId(bookmarkId: deviceUUID.uuidString) else {
-            Swift.print("Tried to update reading session for a book")
-            return
-        }
-        
-        if data == "1" {
-            Task {
-                do {
-                    try await user.startReadingSession(readingBook: readingBook)
-                } catch let error {
-                    print(error)
-                }
-            }
-        }
-        
-        if data == "0" {
-            print("Stoped reading")
-            showAlert()
-            Task {
-                do {
-                    try await user.stopReadingSession(readingBook: readingBook, pages: 10)
-                } catch let error {
-                    print(error)
-                }
             }
         }
     }
